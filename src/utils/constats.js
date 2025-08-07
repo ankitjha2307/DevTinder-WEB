e5ec5b93957f9f12ced38c08ca8a44b4f7fdaad3
ankit <ankitjha0723@gmail.com>
2025-08-08 00:06:07 +0530

    /

--- src/utils/constats.js
+++ src/utils/constats.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://devtinder-2-ng2b.onrender.com";
+export const BASE_URL = "https://devtinder-2-ng2b.onrender.com/";
 export const Background_img =
   "https://tinder.com/static/build/8ad4e4299ef5e377d2ef00ba5c94c44c.webp";
